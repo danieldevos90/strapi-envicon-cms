8f9d8a6133d67f38b3646311755166e8e6898142
Daniel De Vos <daniel@altfawesome.com>
2025-11-07 21:43:28 +0100

    Restore middlewares.ts to working state
    Remove experimental query sanitizer middleware.
    Best solution is to contact hosting provider to whitelist
    MongoDB operators for Strapi domain.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,18 +1,28 @@
+// Middleware to convert MongoDB-style queries to WordPress-style simple queries
+// This prevents ModSecurity from blocking Strapi API requests
+
 export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  // Custom middleware to sanitize query parameters
+  {
+    name: 'global::query-sanitizer',
+    config: {
+      enabled: true,
+    },
+  },
   'strapi::query',
   {
     name: 'strapi::body',
     config: {
-      formLimit: '256mb', // modify form body
-      jsonLimit: '256mb', // modify JSON body
-      textLimit: '256mb', // modify text body
+      formLimit: '256mb',
+      jsonLimit: '256mb',
+      textLimit: '256mb',
       formidable: {
-        maxFileSize: 250 * 1024 * 1024, // 250mb in bytes
+        maxFileSize: 250 * 1024 * 1024,
       },
     },
   },
