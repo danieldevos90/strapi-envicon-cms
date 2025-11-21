a03a9437692f8ab11a8528572f6ebd06cfcab728
Daniel De Vos <daniel@altfawesome.com>
2025-11-21 11:13:28 +0100

    Add news-page content type and news-intro component
    - Created news-page singleType content type with hero, newsIntro, and SEO fields
    - Created news-intro component for intro sections with description, button, and background options
    - Added controller, routes, and services for news-page API

---
+++ src/api/news-page/routes/news-page.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * news-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::news-page.news-page');
+
